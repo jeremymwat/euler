e5db3cf2e134bd06da8410919e4627141ec163d9
Jeremy <jeremymwatson@gmail.com>
2014-06-05 15:22:14 -0400

    remove prime computation of evens

--- 007/prime.c
+++ 007/prime.c
@@ -1,13 +1,11 @@
-// findin primes the lazy way
-// Euler problem 007
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
 
 bool prime (int x)
 {
-    for (int i = 2; i < (sqrt(x) + 1); i++)
+    int i;
+    for (i = 2; i < (sqrt(x) + 1); i++)
     {
         if (x % i == 0 && i != x){return false;}
     }
@@ -17,17 +15,18 @@ bool prime (int x)
 int main (void)
 {
     int count = 0;
-    int checknum = 2;
-    while (count < 10001)
+    int checknum = 3;
+    while (count < 10000)
     {
         if (prime(checknum))
         {
             count++;
-            checknum++;
+            checknum = checknum + 2;
         } else {checknum++;}
 
     }
+    
 
-    printf("%d \n", (checknum-1));
+    printf("%d %f \n", (checknum-1));
     return 0;
 }
